6a567a3e17a6db3f4fa3dd2ba53392c07e357d25
Henry Caron <henryacaron@gmail.com>
2022-03-22 11:43:04 -0400

    created arenamode and createplanet panes in createlobby

---
+++ src/Frontend/Panes/Lobbies/ArenaModePane.tsx
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React from 'react';
+import {
+  Checkbox,
+  DarkForestCheckbox,
+  DarkForestNumberInput,
+  NumberInput,
+} from '../../Components/Input';
+import { Row } from '../../Components/Row';
+
+import { LobbiesPaneProps, Warning } from './LobbiesUtils';
+
+export function ArenaModePane({ config: config, onUpdate: onUpdate }: LobbiesPaneProps) {
+  return (
+    <>
+      <Row>
+        <Checkbox
+          label='Players can only spawn into admin designated spawn planets?'
+          checked={config.MANUAL_SPAWN.displayValue}
+          onChange={(e: Event & React.ChangeEvent<DarkForestCheckbox>) =>
+            onUpdate({ type: 'MANUAL_SPAWN', value: e.target.checked })
+          }
+        />
+      </Row>
+      <Row>
+        <Checkbox
+          label='Players win by capturing admin designated target planets?'
+          checked={config.TARGET_PLANETS.displayValue}
+          onChange={(e: Event & React.ChangeEvent<DarkForestCheckbox>) =>
+            onUpdate({ type: 'TARGET_PLANETS', value: e.target.checked })
+          }
+        />
+      </Row>
+      {config.TARGET_PLANETS.displayValue && (
+        <Row>
+          <span>Blocks between Invade & Claim Victory</span>
+          <NumberInput
+            value={config.TARGET_PLANET_HOLD_BLOCKS_REQUIRED.displayValue}
+            onChange={(e: Event & React.ChangeEvent<DarkForestNumberInput>) => {
+              onUpdate({ type: 'TARGET_PLANET_HOLD_BLOCKS_REQUIRED', value: e.target.value });
+            }}
+          />
+        </Row>
+      )}
+    </>
+  );
+}
